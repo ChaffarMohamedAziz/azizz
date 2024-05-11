52b33d67d3b31068e95eb90d369bd3c503bfeb2e
ChaffarMohamedAziz <chaffaraziz54@gmail.com>
2024-05-11 17:00:38 +0100

    first commit

--- index.js
+++ index.js
@@ -2,56 +2,90 @@ const express = require('express')
 const ejs = require('ejs')
 const path = require('path')
 const fs = require('fs')
-const app  = express()
-app.set('view engine','ejs')
-app.set('views', path.join(__dirname , 'views'))
-app.use(express.urlencoded({extended:true}))
+const app = express()
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 port = 8000
 
-app.get('/',(req,res)=>{
-    res.render('home')
+app.get('/home', (req, res) => {
+  res.render('home')
 })
 app.get('/create', (req, res) => {
-    res.render('create');
-  });
-app.post('/create',(req, res)=>{
-    const {name, age, languages} = req.body
-    const newData = {name,age, languages: Array.isArray(languages) ? languages : [languages]}
-    fs.readFile('data.json', 'utf8', (err, data) => {
-        if(err){
-            console.log(err)
-            res.status(500).send('Internal Server Error')
-            return
-        }
-        const jsonData = data? JSON.parse(data) : []
-        jsonData.push(newData)
-        fs.writeFile('data.json', JSON.stringify(jsonData, null, 2), (err) => {
-            if (err) {
-              console.error(err);
-              res.status(500).send('Internal Server Error');
-              return;
-            }
-            
-            res.render('create', { message: 'Data saved successfully.' })
-          });
-      });
-
-})
-app.get('/data', (req, res) => {
-    fs.readFile('data.json', 'utf8', (err, data) => {
+  res.render('create');
+});
+app.post('/create', (req, res) => {
+  const { name, age, languages } = req.body
+  const newData = { name, age, languages: Array.isArray(languages) ? languages : [languages] }
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err)
+      res.status(500).send('Internal Server Error')
+      return
+    }
+    const jsonData = data ? JSON.parse(data) : []
+    jsonData.push(newData)
+    fs.writeFile('data.json', JSON.stringify(jsonData, null, 2), (err) => {
       if (err) {
         console.error(err);
         res.status(500).send('Internal Server Error');
         return;
       }
-  
-      const jsonData = data ? JSON.parse(data) : [];
-      res.render('data', { data: jsonData });
+
+      res.render('create', { message: 'Data saved successfully.' })
     });
   });
-  
 
-app.listen(port,()=>{
-    console.log(`server listerning on port${port}`)
+})
+app.get('/data', (req, res) => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    const jsonData = data ? JSON.parse(data) : [];
+    res.render('data', { data: jsonData });
+  });
+});
+app.get('/data/age', (req, res) => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    const jsonData = data ? JSON.parse(data) : [];
+    const filteredData = jsonData.filter(data => data.age > 20);
+
+    filteredData.forEach(data => {
+      console.log(`Nom : ${data.name}, Âge : ${data.age}, Langages : ${data.languages.join(', ')}`);
+    });
+
+    res.render('data', { data: filteredData });
+  });
+});
+app.get('/data/java', (req, res) => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    const jsonData = data ? JSON.parse(data) : [];
+    const filteredData = jsonData.filter(data => data.languages.includes("Java"));
+
+    filteredData.forEach(data => {
+      console.log(`Nom : ${data.name}, Âge : ${data.age}, Langages : ${data.languages.join(', ')}`);
+    });
+
+    res.render('data', { data: filteredData });
+  });
+});
+app.listen(port, () => {
+  console.log(`server listerning on port${port}`)
 })
